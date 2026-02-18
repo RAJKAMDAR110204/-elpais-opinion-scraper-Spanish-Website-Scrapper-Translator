2b068bde1ec2ff8555b1fbe9af631cb532c074a5
RAJKAMDAR110204 <121775097+RAJKAMDAR110204@users.noreply.github.com>
2026-02-18 14:46:12 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,55 @@
-from scraper import setup_driver, get_opinion_articles, scrape_article
+from selenium import webdriver
+from scraper import get_opinion_articles, scrape_article
 from translator import translate_title
 from analyzer import analyze_titles
+
+
+def setup_driver():
+    USERNAME = "rajkamdar_oItfYU"
+    ACCESS_KEY = "njUQPzrRu1pDLLRH61ty"
+
+    options = webdriver.ChromeOptions()
+
+    # BrowserStack specific capabilities
+    bstack_options = {
+        "os": "Windows",
+        "osVersion": "11",
+        "sessionName": "ElPais Opinion Scraper Assignment",
+        "buildName": "ElPais Automation Build"
+    }
+
+    options.set_capability("browserName", "Chrome")
+    options.set_capability("browserVersion", "latest")
+    options.set_capability("bstack:options", bstack_options)
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{USERNAME}:{ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub",
+        options=options
+    )
+
+    return driver
 
 
 def main():
     driver = setup_driver()
 
-    article_urls = get_opinion_articles(driver)
+    try:
+        article_urls = get_opinion_articles(driver)
 
-    translated_titles = []
+        translated_titles = []
 
-    for i, url in enumerate(article_urls):
+        for i, url in enumerate(article_urls):
+            driver.get(url)
 
-        driver.get(url)
+            title = scrape_article(driver, i)
 
-        title = scrape_article(driver, i)
+            translated = translate_title(title)
+            translated_titles.append(translated)
 
-        translated = translate_title(title)
-        translated_titles.append(translated)
+        analyze_titles(translated_titles)
 
-    analyze_titles(translated_titles)
-
-    driver.quit()
+    finally:
+        driver.quit()
 
 
 if __name__ == "__main__":
